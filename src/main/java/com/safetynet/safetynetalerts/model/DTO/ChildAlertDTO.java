67f49e9989207400ae16504fe7af0416459e445a
Lthiellaud <laurence.thiellaud@gmail.com>
2021-02-19 08:58:37 +0100

    Model DTO for URL /childAlert added

---
+++ src/main/java/com/safetynet/safetynetalerts/model/DTO/ChildAlertDTO.java
@@ -0,0 +1,10 @@
+package com.safetynet.safetynetalerts.model.DTO;
+
+import java.util.List;
+
+public class ChildAlertDTO {
+
+    private List<PersonAlertDTO> childs;
+    private List<PersonAlertDTO> adults;
+
+}

ed22109d9b6706647f4732271181c6b34ab72165
Lthiellaud <laurence.thiellaud@gmail.com>
2021-03-01 17:55:55 +0100

    URL /firestation?stationNumber : service added

--- src/main/java/com/safetynet/safetynetalerts/model/DTO/PersonAndCountByFireStationDTO.java
+++ src/main/java/com/safetynet/safetynetalerts/model/DTO/PersonAndCountByFireStationDTO.java
@@ -1,10 +1,13 @@
 package com.safetynet.safetynetalerts.model.DTO;
 
+import lombok.Setter;
+
 import java.util.List;
 
+@Setter
 public class PersonAndCountByFireStationDTO {
     private List<PersonByFireStationDTO> personsByFireStation;
-    private int childrenCount;
-    private int adultsCount;
+    private Long childrenCount;
+    private Long adultsCount;
 
 }
